41bf9c945898b273ab40024023784e1dec343db8
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-27 14:19:17 -0300

    feat: cria um gerador de link no link-controller

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -1,7 +1,27 @@
-import { Controller, Get, Post, Req } from "@nestjs/common";
+import { Controller, Post, Req } from "@nestjs/common";
 import { Request } from "express";
 
 const URL = "https://localhost:3000/";
 
 @Controller("link")
-export class LinkController {}
+export class LinkController {
+  @Post("create")
+  createLinks(@Req() request: Request): string {
+    console.log(request.query);
+    return this._createLink();
+  }
+
+  private _createLink(): string {
+    let link: string = "";
+    const possibleChars: string =
+      "abcdefghijklmnopqrstuvwxyz+ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789";
+
+    for (let i = 0; i < 5; i++)
+      link +=
+        possibleChars[
+          Math.floor(Math.random() * (possibleChars.length - 0) + 0)
+        ];
+
+    return URL + link;
+  }
+}
