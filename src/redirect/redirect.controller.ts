e75076be94e7399801f051ef25d13a97f3f98b3d
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-27 20:40:55 -0300

    feat: adicona funções para redirecionar para um link

--- src/redirect/redirect.controller.ts
+++ src/redirect/redirect.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, Redirect, Res } from "@nestjs/common";
+import { LinkCut } from "src/link/models";
+import { Response } from "express";
 
 const fake_redirect = {
-    id: "cdglc",
+    id: "lWvlu",
     bigLink: "www.google.com",
-    smallLink: "https://localhost:3000/cdglc",
+    smallLink: "https://localhost:3000/redirect/lWvlu",
 };
-
 @Controller("redirect")
 export class RedirectController {
-    @Get(":link")
-    redirect(): string {
-        return "";
+    @Get()
+    linkNotFound(): string {
+        return "link not found";
+    }
+
+    @Get("/:id")
+    redirect(@Param() params, @Res() res: Response) {
+        res.status(301).redirect("https://" + this._getLink(params.id).bigLink);
+    }
+
+    private _getLink(id: string): LinkCut {
+        // isso utilizará um serviço
+        return fake_redirect;
     }
 }
