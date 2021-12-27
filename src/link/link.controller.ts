c4da04dfe80fb92f0465674a1f0526f5d2ed4082
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-27 14:18:34 -0300

    feat: adiciona link-controller

---
+++ src/link/link.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Get, Post, Req } from "@nestjs/common";
+import { Request } from "express";
+
+const URL = "https://localhost:3000/";
+
+@Controller("link")
+export class LinkController {}
