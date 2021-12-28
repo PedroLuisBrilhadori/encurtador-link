5431ab92a4e71cabd0169e4ada2b92e83687269d
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-27 22:48:34 -0300

    feat: index para pasta link

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { LinkModule } from "./link/link.module";
+import { LinkModule } from "./link";
 import { RedirectModule } from "./redirect/redirect.module";
 
 @Module({
---
+++ src/link/index.ts
@@ -0,0 +1,4 @@
+export * from "./models";
+export * from "./link.service";
+export * from "./link.module";
+export * from "./link.controller";
