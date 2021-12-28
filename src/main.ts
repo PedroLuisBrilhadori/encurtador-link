e896d7b84c5a3dd5fb9eca4dc730dc12382f75c5
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-27 23:45:14 -0300

    fix: integração com o firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { InitFirebase } from "./firebase.config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+InitFirebase();
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
